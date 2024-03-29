904fff922874401cbeb820c5bf3a9025912dfc52
Paul Maier <github@paulmaier.online>
2024-03-30 00:16:55 +0100

    Added backup stragtegy for uploads

---
+++ backup/scripts/backup-uploads.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+BACKUP_FOLDER=/opt/mediawiki/backup
+NOW=$(date '+%d')
+
+GZIP=$(which gzip)
+
+[ ! -d "$BACKUP_FOLDER" ] && mkdir --parents $BACKUP_FOLDER
+
+FILE=${BACKUP_FOLDER}/backup-${NOW}.tar.gz
+tar -czvf - /uploads | gzip -9 > $FILE
